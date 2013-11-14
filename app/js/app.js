b44249766dc10cff8fd2b5e72e16ce9d162f8c5f
jonnybojangles <blake@solidcobalt.com>
2013-11-14 14:55:51 -0800

    Created some basic factories for testing inheritance purpose

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,11 @@
 'use strict';
 
-angular.module('youtubeWidget',['youtubeWidget.directives']);
+angular.module('youtubeWidget',[
+	'youtubeWidget.directives',
+	'youtubeWidget.factories',
+	'youtubeWidget.config',
+	'youtubeWidget.providers'
+]);
 
 angular.module('youtubeWidget.controllers', []).
 	controller('mainCtrl', ['$scope', function($scope){
@@ -9,16 +14,69 @@ angular.module('youtubeWidget.controllers', []).
 	}]);
 
 angular.module('youtubeWidget.directives', []).
-	directive('youtubeWidgetApp', [function(){
+	directive('youtubeWidgetApp', ['widgetName', 'widgetByLine', 'day', function(widgetName, widgetByLine, day){
 		return {
 			restrict: 'C',
-			template: '<p>This is a directive test: {{test}}</p>',
+			template: '<p>' +
+				'This is a directive test: {{test}}' +
+				'</p>' +
+				'<div data-ng-transclude></div>',
+			transclude: true,
 			controller: function($scope, $element, $attrs, $transclude) {
 				$scope.test = 'Test Complete';
+				$scope.name = widgetName.name;
+				$scope.by = widgetByLine.by;
+				$scope.date = day.day;
+				console.log(widgetName);
+				console.log(widgetByLine);
+				console.log(day);
+				// load style sheet
 			}
 		}
 	}]);
 
-//angular.module('youtubeWidget.services', []).factory(
-//
-//);
+angular.module('youtubeWidget.services', []).
+	service('includeStyle', function(){
+		// Append a style sheet to the head if it has not already
+	});
+
+
+angular.module('youtubeWidget.factories', []).
+	factory('widgetName', function(){
+		return {
+			name: 'YouTube Widget'
+		}
+	});
+
+angular.module('youtubeWidget').
+	factory('widgetVersion', function(){
+		return {
+			version: '0.123'
+		}
+	});
+
+angular.module('youtubeWidget.config', ['youtubeWidget.providers']).
+	config(function($provide){
+		$provide.factory('widgetByLine', function(){
+			return {
+				by: 'Blake'
+			}
+		});
+	}).config(function(dayProvider){
+		dayProvider.setDay('Mon');
+	});
+
+angular.module('youtubeWidget.providers', []).
+	provider('day', function(){
+		var day = '';
+		return {
+			setDay: function(current){
+				day = current;
+			},
+			$get: function(){
+				return {
+					day: day + 'day'
+				}
+			}
+		}
+	});
