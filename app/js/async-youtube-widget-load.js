caf9183e316172a245b3ab101363317c507aa307
jonnybojangles <blake@solidcobalt.com>
2013-11-21 01:26:01 -0800

    Updated .forEach for IE8, built/min'ed

--- app/js/async-youtube-widget-load.js
+++ app/js/async-youtube-widget-load.js
@@ -49,7 +49,15 @@
 //		};
 //	defaultSheet = createCSSheet();
 //	defaultSheet.insertRule('', 0);
+	if ( !Array.prototype.forEach ) {
+		Array.prototype.forEach = function(fn, scope) {
+			for(var i = 0, len = this.length; i < len; ++i) {
+				fn.call(scope, this[i], i, this);
+			}
+		}
+	}
 	styleSheets.forEach(loadCSS);
+
 
 })(document);
 
